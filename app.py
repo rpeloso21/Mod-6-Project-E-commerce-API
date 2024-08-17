ba7ef89723f813e7b01b4888362ed9f156e03156
rpeloso21 <rpeloso21@gmail.com>
2024-08-16 23:04:30 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,220 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from marshmallow import fields
+from marshmallow import ValidationError
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Tsrost007!@localhost/e_commerce_db'
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class CustomerSchema(ma.Schema):
+    id = fields.Integer(required=False)
+    name = fields.String(required=True)
+    email = fields.String(required=True)
+    phone = fields.String(required=True)
+
+class ProductSchema(ma.Schema):
+    id = fields.Integer(required=True)
+    name = fields.String(required=True)
+    price = fields.Float(required=True)
+
+class OrderSchema(ma.Schema):
+    id = fields.Integer(required=True)
+    date = fields.Date(required=True)
+    customer_id = fields.Integer(required=True)
+
+class AccountSchema(ma.Schema):
+    id = fields.Integer(required=True)
+    username = fields.String(required=True)
+    password = fields.String(required=True)
+    customer_id = fields.Integer(required=True)
+
+
+customer_schema = CustomerSchema()
+customers_schema = CustomerSchema(many=True)
+
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+order_schema = OrderSchema()
+orders_schema = OrderSchema(many=True)
+
+account_schema = AccountSchema()
+accounts_schema = AccountSchema(many=True)
+
+
+
+class Customer(db.Model):
+    __tablename__ = 'Customers'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255), nullable = False)
+    email = db.Column(db.String(320))
+    phone = db.Column(db.String(15))
+
+class Product(db.Model):
+    __tablename__ = 'Products'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255), nullable = False)
+    price = db.Column(db.Float, nullable = False)
+
+class Order(db.Model):
+    __tablename__ = 'Orders'
+    id = db.Column(db.Integer, primary_key = True)
+    date = db.Column(db.Date, nullable = False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('Customers.id'))
+
+class CustomerAccount(db.Model):
+    __tablename__ = 'Customer_Accounts'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255), unique = True, nullable = False)
+    password = db.Column(db.String(255), nullable = False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('Customers.id'))
+
+# Routes
+# Customer Routes ---------------------------------------------------------------------------
+
+@app.route('/', methods=['GET'])
+def return_home():
+    return "Home Page"
+
+@app.route('/customers', methods=['GET'])
+def get_all_customers():
+    customers = Customer.query.all()
+    return customers_schema.jsonify(customers)
+
+@app.route('/customers/<int:id>', methods=['GET'])
+def get_customers(id):
+    customer = Customer.query.get_or_404(id)
+    return customer_schema.jsonify(customer)
+
+@app.route('/customers', methods=['POST'])
+def add_customer():
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    new_customer = Customer(name=customer_data['name'], email=customer_data['email'], phone=customer_data['phone'])
+    db.session.add(new_customer)
+    db.session.commit()
+    return jsonify({"message": "New customer added successfully"}), 201
+
+@app.route('/customers/<int:id>', methods=['PUT'])
+def update_customer(id):
+    customer = Customer.query.get_or_404(id)
+    try:
+        customer_data = customer_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    customer.name = customer_data['name']
+    customer.email = customer_data['email']
+    customer.phone = customer_data['phone']
+    db.session.commit()
+    return jsonify({"message": "Customer details updated successfully"}), 200
+
+@app.route('/customers/<int:id>', methods=['DELETE'])
+def delete_customer(id):
+    customer = Customer.query.get_or_404(id)
+    db.session.delete(customer)
+    db.session.commit()
+    return jsonify({"message": "Customer removed successfully"}), 200
+
+# Account Routes ----------------------------------------------------------------
+
+@app.route('/accounts', methods=['POST'])
+def add_account():
+    try:
+        account_data = account_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    new_account = CustomerAccount(id = account_data['id'], username = account_data['username'], password = account_data['password'], customer_id = account_data['customer_id'])
+    db.session.add(new_account)
+    db.session.commit()
+    return jsonify({"message": "New account added successfully"}), 201
+
+@app.route('/accounts/<int:id>', methods=['GET'])
+def get_account(id):
+    account = CustomerAccount.query.get_or_404(id)
+
+    customer = Customer.query.get_or_404(account.customer_id)
+
+    return account_schema.jsonify(account) #customer_schema.jsonify(customer)
+
+@app.route('/accounts/<int:id>', methods=['PUT'])
+def update_account(id):
+    account = CustomerAccount.query.get_or_404(id)
+    try:
+        account_data = account_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    account.id = account_data['id']
+    account.username = account_data['username']
+    account.password = account_data['password']
+    account.customer_id = account_data['customer_id']
+    db.session.commit()
+    return jsonify({"message": "Account details updated successfully"}), 200
+
+@app.route('/accounts/<int:id>', methods=['DELETE'])
+def delete_account(id):
+    account = CustomerAccount.query.get_or_404(id)
+    db.session.delete(account)
+    db.session.commit()
+    return jsonify({"message": "Account removed successfully"}), 200
+
+# Product Routes --------------------------------------------------
+
+@app.route('/products', methods=['GET'])
+def get_all_products():
+    products = Product.query.all()
+    return products_schema.jsonify(products)
+
+@app.route('/products/<int:id>', methods=['GET'])
+def get_product(id):
+    product = Product.query.get_or_404(id)
+    return product_schema.jsonify(product)
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    try:
+        product_data = product_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    new_product = Product(id=product_data['id'], name=product_data['name'], price=product_data['price'])
+    db.session.add(new_product)
+    db.session.commit()
+    return jsonify({"message": "New product added successfully"}), 201
+
+@app.route('/products/<int:id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get_or_404(id)
+    try:
+        product_data = product_schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
+    product.id = product_data['id']
+    product.name = product_data['name']
+    product.price = product_data['price']
+    db.session.commit()
+    return jsonify({"message": "Product details updated successfully"}), 200
+
+@app.route('/products/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get_or_404(id)
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify({"message": "Product removed successfully"}), 200
+
+
+with app.app_context():
+    db.create_all()
+    
+
+if __name__ == '__main__':
+    app.run(debug = True)
